af8691615eead640a7e496dc7857551f3f139b0d
JM Arenas <rns.jey@gmail.com>
2021-09-20 01:27:59 +0800

    Digit combination and message state
    Added Increment and decrement for digit combination
    and message state that will hold system announcements

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const numbers = [0,1,2,3,4,5,6,7,8,9];
   const [code, generateCode] = useState("");
   const [guess, guessWhat] = useState([0,1,2,3]);
+  const [message, giveMsg] = useState("");
 
   function startGame() {
     let secretCode = "";
@@ -29,22 +30,44 @@ function App() {
     }
   }
 
+  function handleClick(e) {
+    if (message !== "You Lose!") {
+      const { id } = e.target
+      let newArr = [...guess]
+      let pos = [...id]
+      let i = pos.pop();
+
+      pos = pos.join("")
+
+      switch (pos) {
+        case "top":
+          newArr[i] = (newArr[i] === 9) ? 0 : (newArr[i] + 1);
+          break;
+        default:
+          newArr[i] = (newArr[i] === 0) ? 9 : (newArr[i] - 1);
+          break;
+      }
+      
+      guessWhat(prevGuess => newArr);
+    }
+  }
+
   return (
     <div className="Main">
       <div>
         <div className="container">
-          <div className="tile top1" id="top0">{otherNumbers("top",0)}</div>
-          <div className="tile top2" id="top1">{otherNumbers("top",1)}</div>
-          <div className="tile top3" id="top2">{otherNumbers("top",2)}</div>
-          <div className="tile top4" id="top3">{otherNumbers("top",3)}</div>
+          <div className="tile top1" id="top0" onClick={handleClick}>{otherNumbers("top",0)}</div>
+          <div className="tile top2" id="top1" onClick={handleClick}>{otherNumbers("top",1)}</div>
+          <div className="tile top3" id="top2" onClick={handleClick}>{otherNumbers("top",2)}</div>
+          <div className="tile top4" id="top3" onClick={handleClick}>{otherNumbers("top",3)}</div>
           <div className="tile num1">{guess[0]}</div>
           <div className="tile num2">{guess[1]}</div>
           <div className="tile num3">{guess[2]}</div>
           <div className="tile num4">{guess[3]}</div>
-          <div className="tile bot1" id="bot0">{otherNumbers("bot",0)}</div>
-          <div className="tile bot2" id="bot1">{otherNumbers("bot",1)}</div>
-          <div className="tile bot3" id="bot2">{otherNumbers("bot",2)}</div>
-          <div className="tile bot4" id="bot3">{otherNumbers("bot",3)}</div>
+          <div className="tile bot1" id="bot0" onClick={handleClick}>{otherNumbers("bot",0)}</div>
+          <div className="tile bot2" id="bot1" onClick={handleClick}>{otherNumbers("bot",1)}</div>
+          <div className="tile bot3" id="bot2" onClick={handleClick}>{otherNumbers("bot",2)}</div>
+          <div className="tile bot4" id="bot3" onClick={handleClick}>{otherNumbers("bot",3)}</div>
         </div>
         <button onClick={startGame}>Game Start!</button>
       </div>
