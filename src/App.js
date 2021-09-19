190a2d715921386beb86f3016ba6e997f9f33e27
JM Arenas <rns.jey@gmail.com>
2021-09-20 00:42:33 +0800

    Added lock digit tile divs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="Main">
-      
+      <div>
+        <div className="container">
+          <div className="tile top1"></div>
+          <div className="tile top2"></div>
+          <div className="tile top3"></div>
+          <div className="tile top4"></div>
+          <div className="tile num1"></div>
+          <div className="tile num2"></div>
+          <div className="tile num3"></div>
+          <div className="tile num4"></div>
+          <div className="tile bot1"></div>
+          <div className="tile bot2"></div>
+          <div className="tile bot3"></div>
+          <div className="tile bot4"></div>
+        </div>
+      </div>
     </div>
   );
 }
